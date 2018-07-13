800ba1283065a725db4c5d5af03786bee3dc9272
wpunit13 <wpunit13@gmail.com>
2018-07-14 00:03:58 +0530

    wrapper function

--- 01_hello/first-file.go
+++ 01_hello/first-file.go
@@ -1,9 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func wrapper() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	increment := wrapper()
+	increment2 := wrapper()
+	fmt.Println(increment())
+	fmt.Println(increment2())
+	fmt.Println(increment2())
 }
