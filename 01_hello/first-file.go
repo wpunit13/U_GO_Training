ec0a89f5e90dd0fdf3abcc38b31c192d558cc2be
wpunit13 <wpunit13@gmail.com>
2018-07-23 23:52:25 +0530

    pushed first file

--- 01_hello/first-file.go
+++ 01_hello/first-file.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
-func wrapper() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x
-	}
-}
+// func wrapper() func() int {
+// 	x := 0
+// 	fmt.Println("memory address of x is ", &x)
+// 	return func() int {
+// 		x++
+// 		return x
+// 	}
+// }
+
+// func main() {
+// 	increment := wrapper()
+// 	increment2 := wrapper()
+// 	//fmt.Println(increment())
+// 	//fmt.Println(increment2())
+// 	//fmt.Println(increment2())
+// }
 
 func main() {
-	increment := wrapper()
-	increment2 := wrapper()
-	fmt.Println(increment())
-	fmt.Println(increment2())
-	fmt.Println(increment2())
+	for i := 0; i <= 51; i++ {
+		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
+	}
+	fmt.Println('d' - 'a' + 10)
+	fmt.Println()
 }
